c168d84c8286e60c29558f97445b879a3c2758ee
limingzhe <limingzhe_1103@163.com>
2022-03-28 16:39:18 +0800

    新建项目

---
+++ liko/src/main/java/com/liko/LikoApplication.java
@@ -0,0 +1,13 @@
+package com.liko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikoApplication.class, args);
+    }
+
+}
